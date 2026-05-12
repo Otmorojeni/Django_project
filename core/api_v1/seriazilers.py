6096f6a9cdcdb916d2ec58a77d58f806aae77796
mashtakov <mashtakov@gmail.com>
2026-05-12 22:24:00 +0300

    Прописаны сериализаторы для моделей

---
+++ core/api_v1/seriazilers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Theme, Brand, Kit, Review
+
+
+class ThemeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Theme
+        fields = '__all__'
+
+
+class BrandSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+
+class KitSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Kit
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
